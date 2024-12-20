497a5a904d40645b57eae3e208c079b20696a136
UrielDonadel <sba24222@student.cct.ie>
2024-12-20 20:05:40 +0000

    Create MultiplicationTable.java
    Create a Scanner to read input from the user

---
+++ MultiplicationTable.java
@@ -0,0 +1,9 @@
+// Task 2: Generating a Multiplication Table Using a 2D Array
+// gitHub: https://github.com/donadelx/CA2-Programming-Math.git
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // Create a Scanner to read input from the user
+        Scanner scanner = new Scanner(System.in);
