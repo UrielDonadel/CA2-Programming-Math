608f8521788c9dbe6255b6a8a237e96456acd869
UrielDonadel <sba24222@student.cct.ie>
2024-12-20 20:12:00 +0000

    Update DiagonalSum.java
    Calculate the sums of the diagonals

--- DiagonalSum.java
+++ DiagonalSum.java
@@ -20,3 +20,10 @@ public class DiagonalSum {
                 matrix[i][j] = scanner.nextInt();
             }
         }
+
+         // Calculate the sums of the diagonals
+        int mainDiagonalSum = 0, secondaryDiagonalSum = 0;
+        for (int i = 0; i < size; i++) {
+            mainDiagonalSum += matrix[i][i];
+            secondaryDiagonalSum += matrix[i][size - 1 - i];
+        }
