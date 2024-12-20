1b26c32de36e5af1fbd94e7da675f70106391511
UrielDonadel <sba24222@student.cct.ie>
2024-12-20 20:17:55 +0000

    Update BubbleSort.java
    Perform Bubble Sort and display the results

--- BubbleSort.java
+++ BubbleSort.java
@@ -18,3 +18,30 @@ public class BubbleSort {
         for (int i = 0; i < size; i++) {
             array[i] = scanner.nextInt();
         }
+
+        // Perform Bubble Sort
+        int swapCount = bubbleSort(array);
+
+        // Display the results
+        System.out.println("Array after sorting:");
+        for (int num : array) System.out.print(num + " ");
+        System.out.println("\nTotal number of swaps: " + swapCount);
+
+        scanner.close();
+    }
+
+    public static int bubbleSort(int[] array) {
+        int swapCount = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapCount++;
+                }
+            }
+        }
+        return swapCount;
+    }
+}
