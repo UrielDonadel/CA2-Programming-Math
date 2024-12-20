810a3e1e01e0bb44652290876c77ba7f4a84e0d8
UrielDonadel <sba24222@student.cct.ie>
2024-12-20 20:10:55 +0000

    Create DiagonalSum.java
    Ask for the size of the square matrix

---
+++ DiagonalSum.java
@@ -0,0 +1,13 @@
+// Task 3: Calculating the Sum of the Diagonals of a Matrix
+// gitHub: https://github.com/donadelx/CA2-Programming-Math.git
+
+import java.util.Scanner;
+
+public class DiagonalSum {
+    public static void main(String[] args) {
+        // Create a Scanner to read input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask for the size of the square matrix
+        System.out.print("Enter the size of the square matrix: ");
+        int size = scanner.nextInt();
