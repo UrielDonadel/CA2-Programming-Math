a4f2be4a621dae8f31a40ab3344ff97863a61971
UrielDonadel <sba24222@student.cct.ie>
2024-12-20 20:01:23 +0000

    .
    Create a Scanner to read input from the user

---
+++ FirstRepeatedElement.java
@@ -0,0 +1,10 @@
+// Task 1: Finding the First Repeated Element in an Array
+// gitHub: https://github.com/donadelx/CA2-Programming-Math.git
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class FirstRepeatedElement {
+    public static void main(String[] args) {
+        // Create a Scanner to read input from the user
+        Scanner scanner = new Scanner(System.in);
